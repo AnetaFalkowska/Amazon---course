dbc96103a4665050d07bdee370888f2d4738d352
Aneta Falkowska <aneta.falkowska@gmail.com>
2023-11-14 21:27:44 +0100

    Add message to add to cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ ${product.name}
 </div>
 
 <div class="product-quantity-container">
-  <select>
+  <select class="js-quantity-selector-${product.id}">
     <option selected value="1">1</option>
     <option value="2">2</option>
     <option value="3">3</option>
@@ -40,7 +40,7 @@ ${product.name}
 
 <div class="product-spacer"></div>
 
-<div class="added-to-cart">
+<div class="added-to-cart js-added-to-cart-${product.id}">
   <img src="images/icons/checkmark.png">
   Added
 </div>
@@ -55,19 +55,30 @@ ${product.name}
 
 document.querySelector(".products-grid").innerHTML = grid;
 
+let addedMessagesTimeouts={}
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
-  button.addEventListener("click", () => {
-    console.log(button.dataset)
-    const productId = button.dataset.productId;
-
+  button.addEventListener("click", () => { 
+    
+    const {productId} = button.dataset;      
+    const quantity = +document.querySelector(`.js-quantity-selector-${productId}`).value
     const index = cart.findIndex(el => el.productId === productId)
-  
     if (index !== -1) {
-        cart[index].quantity ++;
-    } else {cart.push({ productId, quantity: 1 })}
-
+        cart[index].quantity += quantity;
+    } else {cart.push({productId, quantity})}
     let totalQty=0
     cart.forEach(el=> totalQty+=el.quantity)
     document.querySelector(".js-cart-quantity").innerHTML = totalQty
+    const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`)
+    addedMessage.classList.add("added-to-cart-visible")
+    // Check if there's a previous timeout for this
+    // product. If there is, we should stop it.
+    const existingTimer = addedMessagesTimeouts[productId]
+    if (existingTimer) {clearTimeout(existingTimer)}
+    const timer = setTimeout(()=>{addedMessage.classList.remove("added-to-cart-visible")}, 2000)
+    // Save the timeoutId for this product
+    // so we can stop it later if we need to.
+    addedMessagesTimeouts[productId] = timer
   });
+
 });
